fdc276b7b84b7cfa1e2e2eb3e36395801bd0e3e5
crfpr <153901278+crfpr@users.noreply.github.com>
2024-10-15 17:39:14 +0200

    header fixed to top

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ className }) => {
   return (
-    <header className={`bg-white-600 text-slate-900 p-4 h-16 ${className}`}>
+    <header className={`bg-white-600 text-slate-900 p-4 h-16 ${className} fixed top-0`}>
       <h1 className="text-xl font-bold text-center">PollyGlot</h1>
     </header>
   );
